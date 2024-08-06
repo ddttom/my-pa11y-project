de8d5959514fb1abe83d20390b9bf7be13a134c9
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-08-06 18:15:10 +0100

    use debug in content analysis

--- content-analysis.js
+++ content-analysis.js
@@ -1,6 +1,6 @@
 import fs from "fs/promises";
 import path from "path";
-import { formatCsv } from './utils.js'
+import { formatCsv, debug } from './utils.js'
 
 const extractKeywords = (text, count = 5) => {
   const words = text.toLowerCase().match(/\b\w+\b/g) || [];
